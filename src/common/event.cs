41d29dec1b8229366ea1078bf4dab79e79c55df5
Jan Jevčák <janjevcak@seznam.cz>
2024-09-23 14:12:21 +0200

    comments to event classes

--- src/common/event.cs
+++ src/common/event.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace CalendarCommon
 {
+    /// <summary>
+    /// Represents calendar event with its basic and madatory properties.
+    /// </summary>
     public class CalendarEventBasic : IComparable<CalendarEventBasic>
     {
         public int? id {  get; set; }
@@ -17,23 +20,35 @@ namespace CalendarCommon
 
         public bool IsValid() => id != null && name != null && beginning != null && end != null;
 
+        /// <summary>
+        /// Compares the current CalendarEventBasic instance with another instance 
+        /// based on beginning time, end time, and name in that order.
+        /// </summary>
+        /// <param name="other">The other CalendarEventBasic object to compare to.</param>
+        /// <returns>Returns -1 if the current instance is less than the other, 1 if greater, or 0 if equal.</returns>
         public int CompareTo(CalendarEventBasic? other)
         {
+            // If the other is null, this object is considered greater.
             if (other is null) return 1;
 
             int result = beginning?.CompareTo(other.beginning) ?? -1;
             if(result < 0) return -1;
             if(result > 0) return 1;
 
+            //If beginning times are equal, compare end times.
             result = end?.CompareTo(other.end) ?? -1;
             if(result < 0) return -1;
             if(result > 0) return 1;
 
+            //If end times are equeal, compare names.
             result = name?.CompareTo(other.name) ?? -1;
             return result;
         }
     }
 
+    /// <summary>
+    /// Represents a calendar event with all properties.
+    /// </summary>
     public class CalendarEvent : CalendarEventBasic
     {
         public string? eventDescription { get; set; }
